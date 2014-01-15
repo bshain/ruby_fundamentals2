43af6879452c7820523ce65b79329baa081b57d6
bshain <shainbarbara@gmail.com>
2014-01-15 13:59:34 -0500

    look up bananas, displayed the number of items, and looked for the second item.

--- exercise6.rb
+++ exercise6.rb
@@ -8,3 +8,13 @@ end
 grocery_list << "rice"
  
 list(grocery_list)
+
+puts grocery_list.length
+
+if grocery_list.include? ("bananas")
+	puts "You don't need to pick up bananas today."
+else 
+	puts "You need to pick up bananas."
+end
+
+puts grocery_list[1]
